065702b2d79d32eab5bbbf7c8ae8423fc53eef2d
surveyhub <surveyhub@program>
2021-08-03 15:10:50 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,4 +26,30 @@ uberjar(){
   mv out/surveyhub.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar surveyhub.standalone.jar \
+    --name "surveyhub" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments surveyhub.main \
+    --app-version "1" \
+    $J_ARG
+}
+
 "$@"
